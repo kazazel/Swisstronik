8a3312f32764b1bc22a696b69765b1bc3cdd360b
kazazel <ensyldz680@gmail.com>
2024-07-28 15:14:41 +0000

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: ["0x"], // Özel anahtarınız "0x" ile başlar
+    },
+  },
+};
